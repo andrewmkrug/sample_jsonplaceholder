dcc31aa544b6263f66f886bc2f4abc3da8c737ee
Andrew Krug <andrew.m.krug@gmail.com>
2021-05-14 11:31:24 -0400

    Init naught strings test fixture

--- tests/api_test/test_comments.py
+++ tests/api_test/test_comments.py
@@ -1,5 +1,6 @@
 import json, os, pytest, requests
 from dotenv import load_dotenv
+from naughty_string_validator import *
 
 from api.models import Comments, Comment
 from tests.utils import *
@@ -32,3 +33,17 @@ def test_comments_for_post_to_post_comments(id:int):
     assert_status_code(posts_comments.status_code)
     assert json.loads(comments_filter_by_post.content) == json.loads(posts_comments.content)
 # Would write in a test to see if the comments allow for malformed comments
+
+l = get_naughty_string_list()
+
+@pytest.fixture(params=l)
+def fixture(f):
+    return f
+@pytest.fixture(params=l)
+def test_naught_strings_in_comments(comment: str):
+    data = {"postId": 1, "name": "olo", "email": "test@test.com", "body": comment}
+    response = requests.post(f"{os.getenv('BASE_URL')}/posts/", data=json.dumps(data), headers=headers)
+    comment = Comment.parse_raw(response.content)
+    assert response.status_code == 201  # Returns this, should upgrade utils to accept a range of values
+    for key in data:
+        assert comment.dict()[key] == data[key]
